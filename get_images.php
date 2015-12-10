88578dd66f602b83205de108dd0a2fde3c3fa327
Nicole Fisher <ncspence4@gmail.com>
2015-12-10 10:19:46 -0800

    Completed modal and carousel functionality for vendors

--- get_images.php
+++ get_images.php
@@ -1,10 +1,28 @@
 <?php
-$images=glob('images/*');
-$output_data =[
-    'success'=> true,
-    'files'=> $images,
-    'errors' => ['An error has occured']
-];
-$output =json_encode($output_data);
-print($output);
+//$images=glob($_POST['images/floral/*']);
+//$output_data =[
+//    'success'=> true,
+//    'files'=> $images,
+//    'errors' => ['An error has occured']
+//];
+//$output =json_encode($output_data);
+//print($output);
+
+require_once('mysql_connect.php');
+$vendor_id = $_POST['vendor'];
+$query = "SELECT * FROM vendor_photos WHERE vendor_id=$vendor_id";
+
+$result = mysqli_query($conn,$query);
+$output_array = ['success'=>false];
+$photos = [];
+if(mysqli_num_rows($result)>0){
+    while($row = mysqli_fetch_assoc($result)){
+        $photos[] = $row;
+    }
+    $output_array['success']=true;
+    $output_array['images']=$photos;
+} else{
+    $output_array['errors']=mysqli_error($conn);
+}
+print(json_encode($output_array));
 ?>
--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-/**
- * Created by nicolespence on 11/25/15.
- */
 //parallax functionality
 $(window).scroll(function(element){
     parallax();
@@ -12,36 +9,268 @@ function parallax(){
 
 //onload
 $(document).ready(function(){
+    //floralModal();
+    buildGallery();
     createImages();
-    vendorContainers();
 });
 
 
 //DOM creation for bridal images
-function createImages(bridalImages) {
-    var bridalImages= ["images/AndyKing0312.jpg", "images/AndyKing0319.jpg", "images/AndyKing0326.jpg", "images/AndyKing0312.jpg", "images/AndyKing0319.jpg", "images/AndyKing0326.jpg"];
+function buildGallery(){
+    var gallery= [
+        {
+            src: "images/accessories.jpg",
+            text: "Accessories"
 
-    for (var i = 0; i <bridalImages.length; i++) {
-        var image= $('<img>').addClass('bride').attr('src', bridalImages[i]).html("Bride "+[i]);
-        $('.brideContainer').append(image);
+        },
+        {
+            src: "images/makeup.jpg",
+            text: "Hair and Make-Up"
+        },
+        {
+            src: "images/cake.jpg",
+            text: "Cake"
+        },
+        {
+            src: "images/dress.jpg",
+            text: "Attire"
+        },
+        {
+            src: "images/floralVase.jpg",
+            text: "Flowers"
+        },
+        {
+            src: "images/photobooth.jpg",
+            text: "Extras"
+        },
+        {
+            src: "images/photographer.jpg",
+            text: "Photography"
+        },
+        {
+            src: "images/venue.jpg",
+            text: "Venue"
+        },
+        {
+            src: "images/videographer.jpg",
+            text: "Videography"
+        }
+    ];
+    var container = $('.vendorContainer');
+
+    for(var i=0; i<gallery.length; i++){
+
+        var imgContainer = $("<div>", {
+            class: 'img-container',
+            id: 'img-' + i
+        });
+
+        var img = $("<img>", {
+            src: gallery[i].src,
+            class: 'gallery-img'
+        });
+
+        var cover = $("<div>", {
+            class: 'text-cover'
+        });
+
+        var text = $("<div>", {
+            text: gallery[i].text,
+            class: 'text'
+        });
+
+        cover.append(text).hide();
+
+        imgContainer.append(img,cover).appendTo(container);
+
+        (function(){
+            var divId = '#img-' + i;
+            var thisCover = cover;
+
+            container.on("mouseenter", divId, function(){
+                thisCover.fadeIn(500);
+                thisCover.css({
+                    margin: '76px -5px -42px 43px',
+                    width: '20.5vw',
+                    height: '28.7vh',
+                    'max-width': '100%',
+                    'max-height': '100%'
+                });
+            }).on("mouseleave", divId, function(){
+                thisCover.fadeOut(500);
+            });
+        })();
     }
-    $('img').click(function () {
+}
+
+// DOM creation for bridal images under Featured Brides
+function createImages() {
+    var bridalImages = [
+        {
+            src: "images/AndyKing0312.jpg",
+            text: "Nicole Fisher"
+
+        },
+        {
+            src: "images/AndyKing0319.jpg",
+            text: "Chermaine Zimmerman"
+        },
+        {
+            src: "images/AndyKing0326.jpg",
+            text: "Rosanna Spence"
+        }
+        ];
+    for (var i = 0; i < bridalImages.length; i++) {
+        var image = $('<img>', {
+            class: 'bride',
+            src: bridalImages[i].src
+        });
+        image.attr('index', i);
+        $('.brideContainer').append(image);
+        console.log(image);
+        console.log(bridalImages[i].text);
+    }
+    // Opens modal under Featured Brides
+    $('.bride').click(function () {
         $('.modal-body').empty();
         $('#myModal').modal();
-        var imgSource = $(this).attr('src');
-        var fullImg = $('<img>').attr('src', imgSource).css('width', '100%').css('margin', 'auto', '0');
-        $('.modal-body').append(fullImg);
+        var title = $('<h2>', {
+                   text: bridalImages[$(this).attr('index')].text,
+                   class: 'modal-title'
+               });
+            var imgSource = $(this).attr('src');
+            var fullImg = $('<img>').attr('src', imgSource).css('width', '100%').css('margin', 'auto', '0');
+            $('.modal-body').append(fullImg);
+            $('.modal-title').html(title);
+            //console.log(bridalImages[i].text);
 
+    });
+    console.log("moooooooo");
+    $('.text-cover').click(function () {
+        //$('.modal-body').empty();
+        console.log('I can has working now?');
+        get_vendor_images(5); //TODO: NO MAGIC NUMBERS RAWWWRR
+    });
+
+}
+function get_vendor_images(vendor_id){
+    $.ajax({
+        url:'http://localhost:8888/lfz/Just-Married/get_images.php',
+        dataType: 'json',
+        method: 'post',
+        data: {vendor: vendor_id},
+        success: function(response) {
+            if(!response.success){
+                return;
+            }
+            console.log('it dun worked', response.images);
+            $('#imagemodal').modal();
+            //var imgSource = $(this).attr('src');
+            //var fullImg = $('<img>').attr('src', imgSource).css('width', '100%').css('margin', 'auto', '0');
+            //$('.modal-body').append(fullImg);
+            //console.log('text cover working');
+            make_carousel_photos(response.images, '#imagemodal');
+        },
+        error: function(){
+            console.log('something dun fucked up');
+        }
     });
 }
 
-function vendorContainers(){
-    var nicoleVendors = ["images/accessories.jpg", "images/bridesmaids.jpg", "images/cake.jpg", "images/dress.jpg", "images/floral.jpg", "images/photobooth.jpg", "images/photographer.jpg","images/venue.jpg", "images/videographer.jpg"];
-
-    for(var i=0; i<nicoleVendors.length; i++){
-        var vendorDiv = $('<div>').addClass('vendorDiv');
-        var vendorImage= $('<img>').attr('src', nicoleVendors[i]).addClass('vendorImage').addClass('front');
-        $(vendorDiv).append(vendorImage);
-        $('.vendorContainer').append(vendorDiv);
+function make_carousel_photos(photo_array, carousel_id){
+    photo_container=[];
+    photo_indicator=[];
+    for(var i=0; i<photo_array.length;i++){
+        $div = $("<div>",{
+            class:"item"
+        });
+        $span = $("<span>",{
+            text: photo_array[i].caption,
+            class: 'white-text'
+        });
+        $photo = $("<img>",{
+            src: 'images/'+ photo_array[i].file,
+            alt: photo_array[i].caption
+        });
+        $div.append($photo,$span);
+        $indicator = $("<li>",{
+            'data-target': carousel_id,
+            'data-slide-to':i
+        });
+        $title = $("<h4>", {
+            text: photo_array[i].title,
+            class: 'modal-title'
+        });
+        $link = $("<a>",{
+            href: 'www.lisahadleystudios.com',
+            class:'hoverButton',
+        });
+        $button = $('button'),
+        photo_container.push($div);
+        photo_indicator.push($indicator);
     }
+    console.log(photo_container[0]);
+    photo_container[0].addClass('active');
+    photo_indicator[0].addClass('active');
+    $('.modal-title').append($title);
+    $('button')
+
+    $(carousel_id).find('.carousel-indicators').empty().append(photo_indicator);
+    $(carousel_id).find('.carousel-inner').empty().append(photo_container);
+
 }
+
+$('.img-container').on('click','.text-cover');
+$('.text-cover').on('click', function(){
+        console.log('onclick working');
+    });
+
+
+//function floralModal(){
+//   var floralGallery = ['images/floral.jpg', 'images/aisle.jpg', 'images/aisle1.jpg','images/aisle2.jpg', 'images/floral1.jpg']
+//   for(var i=0; i<floralGallery.length; i++){
+//       var floralImg = $('<img>').attr('src', floralGallery[i]);
+//
+//    }
+//}
+
+//$('.img-container').click(function () {
+//    //('.modal-body').empty();
+//    //$('#imagemodal').modal();
+//    //var imgSource = $(this).attr('src');
+//    //var fullImg = $('<img>').attr('src', imgSource).css('width', '100%').css('margin', 'auto', '0');
+//    //$('.modal-body').append(fullImg);
+//    console.log('text cover working');
+//});
+
+
+// Flip bridal party card
+$('#f1_card').click(function() {
+    $('#f1_card').addClass('shadow')
+    var frontFace = $('<div>').addClass('front').addClass('face');
+    var backFace = $('<div>').addClass('back').addClass('face').addClass('center');
+    $('#f1_card').append(frontFace);
+    $('#f1_card').append(backFace);
+});
+$('.hover').mouseover(function() {
+    $('.text').css("visibility","visible");
+});
+
+$('.hover').mouseout(function() {
+    $('.text').css("visibility","hidden");
+});
+
+
+//function floralModal(){
+//    var floralGallery = ['images/floral', 'images/aisle.jpg', 'images/aisle1.jpg','images/aisle2.jpg', 'images/floral1.jpg']
+//    for(var i=0; i<floralGallery.length; i++){
+//        var vendorDiv = $('<div>').addClass('vendorDiv');
+//        var vendorTitle = $('<span>').addClass('text-content');
+//        var vendorTitleContainer = $('<span>').text(vendorNames[i]);
+//        var vendorImage= $('<img>').attr('src', nicoleVendors[i]).addClass('vendorImage');
+//        $(vendorTitle).append(vendorTitleContainer);
+//        $(vendorDiv).append(vendorImage).append(vendorTitle);
+//        //$('.vendorTitle').append(vendorDiv);
+//        $('.vendorContainer').append(vendorDiv);
+//    }
+//}
